9ef1398c1a0b0731edc83ecdeb584d01c04d2bc7
Sblvsk <smr.kolesnikov@gmail.com>
2023-07-11 11:20:34 +0600

    spider & items

--- apteka/items.py
+++ apteka/items.py
@@ -1,12 +1,25 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
+from itemloaders.processors import TakeFirst, MapCompose
 
+
+def removing_spaces(value):
+    return value.strip()
+
+def removing_spaces_in_list(data_list):
+    return removing_spaces(data_list[0])
 
 class AptekaItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    timestamp = scrapy.Field(output_processor=TakeFirst())
+    RPC = scrapy.Field(output_processor=TakeFirst())
+    url = scrapy.Field(output_processor=TakeFirst())
+    title = scrapy.Field(output_processor=TakeFirst())
+    marketing_tags = scrapy.Field(input_processor=MapCompose(removing_spaces_in_list))
+    brand = scrapy.Field(output_processor=TakeFirst())
+    section = scrapy.Field()
+    price_data = scrapy.Field()
+    stock = scrapy.Field(input_processor=MapCompose(removing_spaces))
+    assets = scrapy.Field()
+    metadata = scrapy.Field()
+    variants = scrapy.Field(output_processor=TakeFirst())
--- apteka/spiders/aptekaru.py
+++ apteka/spiders/aptekaru.py
@@ -1,6 +1,12 @@
+from datetime import datetime
+
 import scrapy
 from scrapy.http import HtmlResponse
+from scrapy.loader import ItemLoader
 
+from project.apteka.items import AptekaItem
+
+only_one_variant = 1
 
 class AptekaruSpider(scrapy.Spider):
     name = "aptekaru"
@@ -18,4 +24,29 @@ class AptekaruSpider(scrapy.Spider):
             yield response.follow(link, callback=self.products_parse)
 
     def products_parse(self, response: HtmlResponse):
-        pass
+        loader = ItemLoader(item=AptekaItem(), response=response)
+
+        dt = datetime.now()
+        ts = datetime.timestamp(dt)
+        loader.add_value("timestamp", ts)
+
+        loader.add_xpath("RPC", "//div[@data-product-id]/@data-product-id")
+        loader.add_value("url", response.url)
+        loader.add_xpath("title", "//h1/span/text()")
+        loader.add_xpath("marketing_tags", "//span[contains(@class, 'ui-tag')]/text()")
+        loader.add_xpath("brand", "//div[@itemprop='manufacturer']/span[2]/text()")
+        loader.add_xpath("section", "//a[@itemprop='item']/span/span/text()")
+        loader.add_xpath("price_data", "//span[contains(@class, 'goods-offer-panel')]/text() "
+                                       "| //aside//span[@class='ui-link__text']/span/text()")
+        loader.add_xpath("stock", "//aside//span[@class='ui-link__text']/text()")
+        loader.add_xpath("assets", "//div[contains(@class, 'goods-details-page')]//img/@src")
+        loader.add_xpath("metadata", "//div[@itemprop='manufacturer']/span[1]/text() "
+                                     "| //section[@id='description']//h2/text() "
+                                     "| //section//h2/following-sibling::p//text() "
+                                     "| //section//h3/text() "
+                                     "| //section//h3/following-sibling::p//text() "
+                                     "| //section//h3/following-sibling::ul//text() "
+                                     "| //section//p/following-sibling::p/text()  ")
+        loader.add_value("variants", only_one_variant)
+
+        yield loader.load_item()
